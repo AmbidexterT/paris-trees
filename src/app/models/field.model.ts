e598c67c0b5b9d7e96f90298ec7703214fc31062
Stepan <Stepan.Petrov@noveogroup.com>
2022-02-24 13:14:33 +0300

    fix: field interface

--- src/app/models/field.model.ts
+++ src/app/models/field.model.ts
@@ -1,14 +1,4 @@
 export interface FieldInterface {
-  typeemplacement: string;
-  domanialite: string;
   arrondissement: string;
-  libellefrancais: string;
   genre: string;
-  espece: string;
-  varieteoucultivar: string;
-  circonferenceencm: number;
-  hauteurenm: number;
-  stadedeveloppement: string;
-  remarquable: string;
-  geo_point_2d: Array<number>;
 }
